74f3bdba42e160052ed7a2574fc4e399edcac3a3
zeus <z@stucom.com>
2016-03-02 17:03:26 +0100

    Variable nombre

--- Hola.java
+++ Hola.java
@@ -1,5 +1,8 @@
 public class Hola {
+
+  private static String nombre = "zeus";
+
   public static void main(String[] args) {
-    System.out.println("hola zeus");
+    System.out.println("hola " + nombre);
   }
 }
