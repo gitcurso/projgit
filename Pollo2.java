fdefb1472d65fa75fffa18c8e4f30bf3bbcb54c8
zeus <z@stucom.com>
2016-03-03 17:03:01 +0100

    Más pollos

--- Pollo2.java
+++ Pollo2.java
@@ -2,7 +2,8 @@ public class Pollo {
   private double peso;
   private String nombre = "Calimero";
  
-  public Pollo(double p) {
+  public Pollo(double p, String n) {
     peso = p;
+    nombre = n;
   }
 }
